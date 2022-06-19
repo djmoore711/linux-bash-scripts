856c414596d645405e4bcd61e587f02ba6cf0de4
DJ Moore <djmoore7.11@gmail.com>
2022-06-19 04:49:28 -0500

    Update install_webmin_debian_ubuntu.sh
    Made it pretty and corrected some more bugs.

--- install_webmin_debian_ubuntu.sh
+++ install_webmin_debian_ubuntu.sh
@@ -7,9 +7,20 @@
 #
 ##############################################################################
 
-#### Function to install WebMin on Ubuntu
+##### Function to install WebMin on Ubuntu
 install () {
-    echo "WebMin is not installed, proceeding with installation..."
+    echo " "
+    echo "##############################################################################"
+    echo " "
+    echo "Welcome to the automated install of WebMin "
+    echo " "
+    echo "This script will check for dependencies, install neccessary components,"
+    echo "add a the WebMin repo to APT, import the required key, and install WebMin"
+    echo " "
+    echo "Proceeding with installation... "
+    echo " "
+    echo "##############################################################################"
+
 
     ##### Check for dependencies and install them if required
     WGET=$(which wget)
@@ -17,17 +28,34 @@ install () {
 
     # WGET check and install
     if test -f "$WGET"; then
+        echo "##############################################################################"
+        echo " "
         echo "$WGET is already installed."
+        echo " "
+        echo "##############################################################################"
+
     else
+        echo "##############################################################################"
+        echo " "
         echo "Installing wget"
+        echo " "
+        echo "##############################################################################"
         apt install wget -y
     fi
 
     # GPG check and install
     if test -f "$GPG"; then
+        echo "##############################################################################"
+        echo " "
         echo "$GPG is already installed."
+        echo " "
+        echo "##############################################################################"
     else
+        echo "##############################################################################"
+        echo " "
         echo "Installing gpg"
+        echo " "
+        echo "##############################################################################"
         apt install gpg -y
     fi
 
@@ -36,17 +64,24 @@ install () {
     FILE=/etc/apt/sources.list
     DIR=/etc/apt/sources.list.d
     if test -f "$FILE"; then
-        echo "$FILE exists."
+        echo "##############################################################################"
+        echo " "
+        echo "The $FILE file exists; adding repo to APT here."
         {
                 echo " "
                 echo "# Adding repository for WebMin"
                 echo "deb [signed-by=/usr/share/keyrings/jcameron-key-archive-keyring.gpg]"
                 echo "https://download.webmin.com/download/repository sarge contrib"
                 } >> "$FILE"
+        echo "The $FILE file has been updated."
+        echo " "
+        echo "##############################################################################"
     else
-        echo "$FILE does not exist, adding repository another way."
+        echo "##############################################################################"
+        echo " "
+        echo "The $FILE file does not exist, adding repository manually."
         mkdir -p "$DIR"
-        cd "$DIR/" || { echo "Failure"; exit 1; }
+        cd "$DIR/" || { echo "Could not move to $DIR"; exit 1; }
         touch "sources.list"
         {
             echo " "
@@ -54,18 +89,47 @@ install () {
             echo "deb [signed-by=/usr/share/keyrings/jcameron-key.gpg]"
             echo "https://download.webmin.com/download/repository sarge contrib"
             } >> "$DIR/sources.list"
+        echo " "
+        echo "The $DIR/sources.list file has been created and updated."
+        echo " "
+        echo "##############################################################################"
+        echo " "
     fi
 
     ##### Installing GPG key for jcameron
-    cd ~ || { echo "Failure"; exit 1; }
-    wget -O- https://download.webmin.com/jcameron-key.asc |
+    echo "##############################################################################"
+    echo "Now importing public key for WebMin repository"
+    cd ~ || { echo "Could not move to home directory"; exit 1; }
+    wget https://download.webmin.com/jcameron-key.asc
+    cat ./jcameron-key.asc |
     gpg --dearmor |
     sudo tee /usr/share/keyrings/webmin-jcameron-archive-keyring.gpg
 
+    ##### Check if key was installed
+    KEY=/usr/share/keyrings/webmin-jcameron-archive-keyring.gpg
+    if test -f "$KEY"; then
+        echo " "
+        echo "Key successfully imported! Continuing with installation."
+        echo " "
+        echo "##############################################################################"
+
+    else
+        echo " "
+        { echo "Key was not successfully imported. Exiting install."; exit 1; }
+        echo " "
+        echo "##############################################################################"
+    fi
+
     ##### Update ATP and install WebMin
+    echo " "
+    echo "##############################################################################"
+    echo "Now updating and installing WebMin. Please wait."
     apt install apt-transport-https
     apt update
-    apt install webmin
+    apt install webmin -y
+    echo " "
+    echo " "
+    echo " "
 
     ##### Checking install
     WMIN=$(which webmin)
@@ -73,10 +137,12 @@ install () {
     # WebMin check
     if test -f "$WMIN"; then
         echo "WebMin installed properly! Now exiting."
+        echo "##############################################################################"
         exit
     else
         echo "Something went wrong with the install process"
         echo "Visit https://www.webmin.com/deb.html for more information"
+        echo "##############################################################################"
         exit
     fi
 }
@@ -84,7 +150,9 @@ install () {
 ##### Checking if WebMin is already installed
 WMINCHK=$(which webmin)
 if test -f "$WMINCHK"; then
+    echo " "
     echo "WebMin is already installed. Now exiting..."
+    echo " "
     exit
 else
     install
