b7b75c7c07975e66b59d32084599cc08627feb3a
DJ Moore <djmoore7.11@gmail.com>
2023-04-01 00:55:13 -0500

    Update downloaded_file_cleanup.sh
    Refactored the script with ChatGPT.

--- downloaded_file_cleanup.sh
+++ downloaded_file_cleanup.sh
@@ -1,108 +1,97 @@
 #!/bin/bash
 
 # Function that moves the files to the parent directory
-function moveFiles {
+function move_files {
     echo
-    echo "Moving files... with the .$filetype extension."
-    echo "--------------------------------------------------------------------------------"
+    echo "Moving .$filetype files to the parent directory..."
+    echo "--------------------------------------------------"
     find . -name "*.$filetype" -execdir mv -v {} ../ \;
 }
 
 # Asking the user to proceed with moving files
-function askMoveFiles {
+function ask_move_files {
+    read -p 'What filetype do you want to move up a directory? (e.g., mp4, avi, jpg): ' filetype
     echo
-    read -p 'What filetype do you want to move up a directory? (eg., mp4, avi, jpg): ' filetype
-    echo 
-    read -p "Are you ready to move all of the .$filetype files? (Y/n) " yn
-    echo 
-    while true; do
-        case $yn in 
-            [yY] ) 
-                echo
-                echo "Your files will be moved now.";
-                echo
-                moveFiles;
-                menu;
-                break;;
-            [nN] ) 
-                echo
-                echo "No files will be moved. Exiting.";
-                echo "--------------------------------------------------------------------------------"
-                exit;;
-            * ) 
-                echo
-                echo "You pressed the wrong key. Try again."
-                echo "--------------------------------------------------------------------------------";;
-        esac
-    done
+    read -p "Are you ready to move all .$filetype files? (Y/n) " answer
+    echo
+
+    case "$answer" in
+        [yY]*)
+            echo "Moving files..."
+            move_files
+            menu
+            ;;
+        [nN]*)
+            echo "No files will be moved. Exiting."
+            echo "--------------------------------------------------"
+            exit
+            ;;
+        *)
+            echo "Invalid answer. Please answer with 'y' or 'n'."
+            ask_move_files
+            ;;
+    esac
 }
 
 # Function that deletes the now empty parent folders
-function delFolders {
+function delete_folders {
     echo
-    echo "Deleting child-level folders now."
-    echo "--------------------------------------------------------------------------------"
+    echo "Deleting child-level folders now..."
+    echo "--------------------------------------------------"
     rm -fr */
 }
 
 # Asking the user to proceed with deleting the now empty folders
-function askDelFolders {
-    while true; do
-        echo 
-        read -p "Are you ready to delete the old, empty folders? (Y/n) " yn
-        echo 
-        case $yn in 
-            [yY] ) 
-                echo
-                echo "The folders will be deleted now.";
-                echo
-                delFolders;
-                echo
-                echo "Folders deleted!"
-                echo "--------------------------------------------------------------------------------"
-                menu;
-                break;;
-            [nN] ) 
-                echo
-                echo "No files will be moved. Exiting.";
-                echo "--------------------------------------------------------------------------------"
-                exit;;
-            * ) 
-                echo
-                echo "You pressed the wrong key. Try again."
-                echo "--------------------------------------------------------------------------------";;
+function ask_delete_folders {
+    read -p "Are you ready to delete the old, empty folders? (Y/n) " answer
+    echo
+
+    case "$answer" in
+        [yY]*)
+            echo "Deleting folders..."
+            delete_folders
+            echo "Folders deleted!"
+            echo "--------------------------------------------------"
+            menu
+            ;;
+        [nN]*)
+            echo "No folders will be deleted. Exiting."
+            echo "--------------------------------------------------"
+            exit
+            ;;
+        *)
+            echo "Invalid answer. Please answer with 'y' or 'n'."
+            ask_delete_folders
+            ;;
+    esac
+}
+
+function menu {
+    PS3="Select a menu number: "
+    echo "--------------------------------------------------"
+    echo "| This script is designed to pull specific       |"
+    echo "| filetypes from all child folders in a directory,|"
+    echo "| move them into the parent directory,            |"
+    echo "| and then delete the old empty child folders.   |"
+    echo "--------------------------------------------------"
+    select option in "Move Files" "Delete Folders" "Quit"; do
+        case "$option" in
+            "Move Files")
+                ask_move_files
+                ;;
+            "Delete Folders")
+                ask_delete_folders
+                ;;
+            "Quit")
+                echo "Quitting the script..."
+                echo "--------------------------------------------------"
+                exit
+                ;;
+            *)
+                echo "Invalid selection. Please choose 1, 2, or 3."
+                ;;
         esac
     done
 }
 
-function menu {
-PS3="Select a menu number: "
-echo "--------------------------------------------------------------------------------"
-echo "| This script is designed to pull specific filetypes from all child folders    |"
-echo "| in a directory, move them into the parent directory, and then delete the old |"
-echo "| empty child folders.                                                         |"
-echo "--------------------------------------------------------------------------------"
-choice=("Move Files" "Delete Folders" "Quit")
-select i in "${choice[@]}" ; do
-    case $i in
-        "Move Files" )
-            askMoveFiles;
-            ;;
-        "Delete Folders" )
-            askDelFolders;
-            ;;
-        "Quit" )
-            echo
-            echo "Quitting the script..."
-            echo
-            exit;;
-        * )
-            echo
-            echo "Invalid Selection: $REPLY"
-            echo
-            break;;
-    esac
-done 
-}
-
-menu 
+menu
